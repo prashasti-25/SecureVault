4a10a4e39bdefa82f926e707ee179613d12ed31e
Prashasti Bhatt <prashastibhatt533@gmail.com>
2026-06-09 11:24:06 +0530

    Setup database and user model

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 
+from app.database.database import engine
+from app.models.user import User
+from app.routers.auth import router as auth_router
+
+User.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+app.include_router(auth_router)
 
 @app.get("/")
 def home():
